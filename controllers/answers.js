f1f0be5bff553e386c7de04085a9f0b6a234dd46
Justin Umberger <justin.umberger@gmail.com>
2020-12-09 17:35:59 -0500

    updated controller catch statements to respond with status of 400 or 500

--- controllers/answers.js
+++ controllers/answers.js
@@ -3,7 +3,7 @@ const models = require('../models');
 module.exports = {
   get: (req, res) => {
     models.answers.get(req.params, req.query)
-      .then(({ rows }) => res.send(
+      .then(({ rows }) => res.status(200).send(
         { 
           'question': req.params['question_id'], 
           'page': req.query['page'] || 1, 
@@ -11,25 +11,25 @@ module.exports = {
           'results': rows 
         }
       ))
-      .catch(err => console.log(err));
+      .catch(() => res.status(500).send('Problem with answers'));
   },
 
   post: (req, res) => {
     models.answers.post(req.params, req.query, Date.now() / 1000)
       .then(({ rows }) => models.photos.post(rows[0], req.query))
       .then(() => res.status(201).send('CREATED'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(400).send('Problem with post'));
   },
 
   helpfulness: (req, res) => {
     models.answers.helpfulness(req.params)
       .then(() => res.status(204).send('NO CONTENT'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(500).send());
   },
 
   report: (req, res) => {
     models.answers.report(req.params)
       .then(() => res.status(204).send('NO CONTENT'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(500).send());
   }
 };
--- controllers/questions.js
+++ controllers/questions.js
@@ -3,25 +3,25 @@ const models = require('../models');
 module.exports = {
   get: (req, res) => {
     models.questions.get(req.query)
-      .then(({ rows }) => res.send({'product_id': req.query['product_id'], 'results': rows }))
-      .catch(err => console.log(err));
+      .then(({ rows }) => res.status(200).send({'product_id': req.query['product_id'], 'results': rows }))
+      .catch(() => res.status(500).send('Error Getting Data'));
   },
 
   post: (req, res) => {
     models.questions.post(req.query, Date.now() / 1000)
       .then(() => res.status(201).send('CREATED'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(400).send('Error with post request'));
   },
 
   helpfulness: (req, res) => {
     models.questions.helpfulness(req.params)
       .then(() => res.status(204).send('NO CONTENT'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(500).send());
   },
 
   report: (req, res) => {
     models.questions.report(req.params)
       .then(() => res.status(204).send('NO CONTENT'))
-      .catch(err => console.log(err));
+      .catch(() => res.status(500).send());
   }
 };
